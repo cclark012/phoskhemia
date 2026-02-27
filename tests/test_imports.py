011848fea18698a55d3583c86e59de2e50d8a5a9
cclark012 <109385164+cclark012@users.noreply.github.com>
2026-02-26 17:18:36 -0700

    test: update test_imports to respect optional dependencies.

--- tests/test_imports.py
+++ tests/test_imports.py
@@ -1,11 +1,12 @@
 import importlib
+import pytest
 
 def test_import_phoskhemia():
-    import phoskhemia  # noqa: F401
+    import phoskhemia  # noqa
 
-def test_import_key_modules():
-    importlib.import_module("phoskhemia.data")
-    importlib.import_module("phoskhemia.fitting")
-    importlib.import_module("phoskhemia.fitting.global_fit")
-    importlib.import_module("phoskhemia.simulation.transient_absorption")
-    importlib.import_module("phoskhemia.visualization.plotting")
+@pytest.mark.parametrize("mod", [
+    "phoskhemia.data",
+    "phoskhemia.fitting.global_fit",
+])
+def test_import_module(mod):
+    importlib.import_module(mod)
