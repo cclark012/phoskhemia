77830df373192971ae84b80c7a61008746f6687d
cclark012 <109385164+cclark012@users.noreply.github.com>
2026-03-10 10:50:56 -0600

    refactor: Add type hints and rename colormaps to not override matplotlib versions. New colormaps all start with "cb2_" to reflect their origin from colorbrewer2.org.

--- phoskhemia/visualization/colormaps.py
+++ phoskhemia/visualization/colormaps.py
@@ -5,31 +5,31 @@ import matplotlib as mpl
 import matplotlib.pyplot as plt
 from matplotlib.colors import LinearSegmentedColormap
 
-nodes = [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0]
+nodes: list[float] = [0.0, 0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875, 1.0]
 
-colors = ['#fcfbfd', '#efedf5', '#dadaeb', '#bcbddc', '#9e9ac8', '#807dba', '#6a51a3', '#54278f', '#3f007d']
-purples = LinearSegmentedColormap.from_list('purples', list(zip(nodes, colors)), N=256)
-mpl.colormaps.register(cmap=purples)
+colors: list[str] = ['#fcfbfd', '#efedf5', '#dadaeb', '#bcbddc', '#9e9ac8', '#807dba', '#6a51a3', '#54278f', '#3f007d']
+cb2_purples = LinearSegmentedColormap.from_list('cb2_purples', list(zip(nodes, colors)), N=256)
+mpl.colormaps.register(cmap=cb2_purples)
 
-colors = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c', '#08306b']
-blues = LinearSegmentedColormap.from_list('blues', list(zip(nodes, colors)), N=256)
-mpl.colormaps.register(cmap=blues)
+colors: list[str] = ['#f7fbff', '#deebf7', '#c6dbef', '#9ecae1', '#6baed6', '#4292c6', '#2171b5', '#08519c', '#08306b']
+cb2_blues = LinearSegmentedColormap.from_list('cb2_blues', list(zip(nodes, colors)), N=256)
+mpl.colormaps.register(cmap=cb2_blues)
 
-colors =  ['#f7fcf5', '#e5f5e0', '#c7e9c0', '#a1d99b', '#74c476', '#41ab5d', '#238b45', '#006d2c', '#00441b']
-greens = LinearSegmentedColormap.from_list('greens', list(zip(nodes, colors)), N=256)
-mpl.colormaps.register(cmap=greens)
+colors: list[str] =  ['#f7fcf5', '#e5f5e0', '#c7e9c0', '#a1d99b', '#74c476', '#41ab5d', '#238b45', '#006d2c', '#00441b']
+cb2_greens = LinearSegmentedColormap.from_list('cb2_greens', list(zip(nodes, colors)), N=256)
+mpl.colormaps.register(cmap=cb2_greens)
 
-colors = ['#fff5eb', '#fee6ce', '#fdd0a2', '#fdae6b', '#fd8d3c', '#f16913', '#d94801', '#a63603', '#7f2704']
-oranges = LinearSegmentedColormap.from_list('oranges', list(zip(nodes, colors)), N=256)
-mpl.colormaps.register(cmap=oranges)
+colors: list[str] = ['#fff5eb', '#fee6ce', '#fdd0a2', '#fdae6b', '#fd8d3c', '#f16913', '#d94801', '#a63603', '#7f2704']
+cb2_oranges = LinearSegmentedColormap.from_list('cb2_oranges', list(zip(nodes, colors)), N=256)
+mpl.colormaps.register(cmap=cb2_oranges)
 
-colors = ['#fff5f0', '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a', '#ef3b2c', '#cb181d', '#a50f15', '#67000d']
-reds = LinearSegmentedColormap.from_list('reds', list(zip(nodes, colors)), N=256)
-mpl.colormaps.register(cmap=reds)
+colors: list[str] = ['#fff5f0', '#fee0d2', '#fcbba1', '#fc9272', '#fb6a4a', '#ef3b2c', '#cb181d', '#a50f15', '#67000d']
+cb2_reds = LinearSegmentedColormap.from_list('cb2_reds', list(zip(nodes, colors)), N=256)
+mpl.colormaps.register(cmap=cb2_reds)
 
-colors = ['#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#000000']
-grays = LinearSegmentedColormap.from_list('grays', list(zip(nodes, colors)), N=256)
-mpl.colormaps.register(cmap=grays)
+colors: list[str] = ['#ffffff','#f0f0f0','#d9d9d9','#bdbdbd','#969696','#737373','#525252','#252525','#000000']
+cb2_grays = LinearSegmentedColormap.from_list('cb2_grays', list(zip(nodes, colors)), N=256)
+mpl.colormaps.register(cmap=cb2_grays)
 
 def plot_linearmap(cmap):
     rgba = cmap(np.linspace(0, 1, 256))
