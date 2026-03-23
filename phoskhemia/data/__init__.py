9204fcf26c960ce2b038c509eccca10d966e47e8
cclark012 <109385164+cclark012@users.noreply.github.com>
2026-03-23 11:47:01 -0600

    feat: Lazy import all common spectrum types to allow for imports from data rather than calling the specific data.module.

--- phoskhemia/data/__init__.py
+++ phoskhemia/data/__init__.py
@@ -1,13 +1,52 @@
 from __future__ import annotations
 from typing import TYPE_CHECKING
 
-__all__ = ["TransientAbsorption"]
+__all__ = [
+    "TransientAbsorption",
+    "Spectrum1D",
+    "FluorescenceSpectrum",
+    "AbsorptionSpectrum",
+    "SpectrumEntry",
+    "SpectrumCollection",
+    "FluorescenceCollection",
+    "AbsorptionCollection",
+    "load_fluorescence_spectrum",
+    "load_absorption_collection",
+]
 
 if TYPE_CHECKING:
-    from phoskhemia.data.spectrum_handlers import TransientAbsorption as TransientAbsorption
+    from phoskhemia.data.spectrum2d import TransientAbsorption as TransientAbsorption
 
 def __getattr__(name: str):
     if name == "TransientAbsorption":
-        from phoskhemia.data.spectrum_handlers import TransientAbsorption
+        from phoskhemia.data.spectrum2d import TransientAbsorption
         return TransientAbsorption
+    if name == "Spectrum1D":
+        from .spectrum1d import Spectrum1D
+        return Spectrum1D
+    if name == "FluorescenceSpectrum":
+        from .spectrum1d import FluorescenceSpectrum
+        return FluorescenceSpectrum
+    if name == "AbsorptionSpectrum":
+        from .spectrum1d import AbsorptionSpectrum
+        return AbsorptionSpectrum
+    if name == "SpectrumEntry":
+        from .spectrum_collections import SpectrumEntry
+        return SpectrumEntry
+    if name == "SpectrumCollection":
+        from .spectrum_collections import SpectrumCollection
+        return SpectrumCollection
+    if name == "FluorescenceCollection":
+        from .spectrum_collections import FluorescenceCollection
+        return FluorescenceCollection
+    if name == "AbsorptionCollection":
+        from .spectrum_collections import AbsorptionCollection
+        return AbsorptionCollection
+    if name == "load_fluorescence_spectrum":
+        from .spectrum1d import load_fluorescence_spectrum
+        return load_fluorescence_spectrum
+    # if name == "load_absorption_spectrum":
+    #     from .spectrum1d import load_absorption_spectrum
+    #     return load_absorption_spectrum
+
     raise AttributeError(f"module {__name__!r} has no attribute {name!r}")
